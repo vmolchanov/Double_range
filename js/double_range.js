b543d4a89441064c2f55c06e36403bf10141cb79
vmolchanov <Molchanov96@yandex.ru>
2017-03-27 00:47:36 +0300

    Added change of range by input

--- js/double_range.js
+++ js/double_range.js
@@ -26,15 +26,67 @@
 
         var self = this;
 
-            // установка значения по умолчанию при загрузке странице
+        // установка значения по умолчанию при загрузке странице
         this._minOutput.value = this._min;
         this._maxOutput.value = this._max;
 
-        // this._output.oninput = function() {
-        //     if (self._inputValidate()) {
-        //         self._setBtnPosition(self._minOutput.value);
-        //     }
-        // };
+        // изменение позиции кнопки минимального значения при изменении значения
+        // в _minOutput
+        this._minOutput.oninput = function() {
+            if (self._inputValidate()) {
+                self._setBtnPosition(self._minBtn, self._minOutput.value);
+
+                var minBtnLeft = self._minBtn.getBoundingClientRect().left;
+                var maxBtnLeft = self._maxBtn.getBoundingClientRect().left;
+                var minBtnWidth = self._minBtn.getBoundingClientRect().width;
+                var leftLineCoord = self._lineLeft.getBoundingClientRect().left;
+
+                var middleLineWidth = maxBtnLeft - minBtnLeft;
+                var leftLineWidth = minBtnLeft - leftLineCoord + minBtnWidth / 2;
+
+                if (middleLineWidth < 0) {
+                    // переопределяем ширину относительно книпки максимального значения
+                    leftLineWidth = maxBtnLeft - leftLineCoord;
+
+                    self._lineLeft.style.width = leftLineWidth + "px";
+                    self._lineMiddle.style.width = Math.abs(middleLineWidth) + "px";
+                    self._lineRight.style.width = lineWidth - Math.abs(middleLineWidth) - leftLineWidth - minBtnWidth / 2 + "px";
+                } else {
+                    self._lineMiddle.style.width = middleLineWidth + "px";
+                    self._lineLeft.style.width = leftLineWidth + "px";
+                    self._lineRight.style.width = lineWidth - leftLineWidth - middleLineWidth + "px";
+                }
+            }
+        };
+
+        // изменение позиции кнопки максимального значения при изменении значения
+        // в _maxOutput
+        this._maxOutput.oninput = function() {
+            if (self._inputValidate()) {
+                self._setBtnPosition(self._maxBtn, self._maxOutput.value);
+
+                var minBtnLeft = self._minBtn.getBoundingClientRect().left;
+                var maxBtnLeft = self._maxBtn.getBoundingClientRect().left;
+                var maxBtnWidth = self._maxBtn.getBoundingClientRect().width;
+                var leftLineCoord = self._lineLeft.getBoundingClientRect().left;
+
+                var middleLineWidth = maxBtnLeft - minBtnLeft;
+                var rightLineWidth = lineWidth - (maxBtnLeft - leftLineCoord) - maxBtnWidth / 2;
+
+                if (middleLineWidth < 0) {
+                    // переопределяем ширину относительно книпки минимального значения
+                    rightLineWidth = lineWidth - (minBtnLeft - leftLineCoord) - maxBtnWidth / 2;
+
+                    self._lineLeft.style.width = maxBtnLeft - leftLineCoord + maxBtnWidth / 2 + "px";
+                    self._lineMiddle.style.width = Math.abs(middleLineWidth) + "px";
+                    self._lineRight.style.width = rightLineWidth + "px";
+                } else {
+                    self._lineMiddle.style.width = middleLineWidth + "px";
+                    self._lineRight.style.width = rightLineWidth + "px";
+                    self._lineLeft.style.width = lineWidth - rightLineWidth - middleLineWidth + "px";
+                }
+            }
+        };
 
         // поведение кнопки минимального значения ползунка
         this._minBtn.onmousedown = function (event) {
@@ -42,10 +94,14 @@
                 var lineLeftCoordX = self._lineLeft.getBoundingClientRect().left;
                 var coordX = event.pageX - lineLeftCoordX;
                 var maxBtnCoordX = self._maxBtn.getBoundingClientRect().left - lineLeftCoordX;
-                var minBtnWidth = self._minBtn.getBoundingClientRect().width;
 
-                var middleLineWidth = maxBtnCoordX - coordX;
-                var leftLineWidth = lineWidth - middleLineWidth - self._lineRight.getBoundingClientRect().width;
+                var minBtnLeft = self._minBtn.getBoundingClientRect().left;
+                var maxBtnLeft = self._maxBtn.getBoundingClientRect().left;
+                var minBtnWidth = self._minBtn.getBoundingClientRect().width;
+                var leftLineCoord = self._lineLeft.getBoundingClientRect().left;
+
+                var middleLineWidth = maxBtnLeft - minBtnLeft;
+                var leftLineWidth = minBtnLeft - leftLineCoord + minBtnWidth / 2;
 
                 if (coordX + minBtnWidth <= maxBtnCoordX) {
                     if (coordX > 0) {
@@ -60,8 +116,9 @@
 
                     self._moveAt(self._minBtn, coordX, lineWidth);
                 } else {
-                    self._lineMiddle.style.width = "0";
-                    self._lineLeft.style.width = lineWidth - self._lineRight.getBoundingClientRect().width + "px";
+                    self._lineMiddle.style.width = middleLineWidth + "px";
+                    self._lineLeft.style.width = leftLineWidth + "px";
+                    self._lineRight.style.width = lineWidth - leftLineWidth - middleLineWidth + "px";
 
                     self._minBtn.style.left = maxBtnCoordX - minBtnWidth + "px";
                 }
@@ -81,15 +138,20 @@
                 var lineLeftCoordX = self._lineLeft.getBoundingClientRect().left;
                 var coordX = event.pageX - lineLeftCoordX;
                 var minBtnCoordX = self._minBtn.getBoundingClientRect().left - lineLeftCoordX;
-                var maxBtnWidth = self._maxBtn.getBoundingClientRect().width;
 
-                var middleLineWidth = coordX - maxBtnWidth / 2 - minBtnCoordX;
-                var leftLineWidth = lineWidth - middleLineWidth - self._lineLeft.getBoundingClientRect().width;
+                var minBtnLeft = self._minBtn.getBoundingClientRect().left;
+                var maxBtnLeft = self._maxBtn.getBoundingClientRect().left;
+                var maxBtnWidth = self._maxBtn.getBoundingClientRect().width;
+                var leftLineCoord = self._lineLeft.getBoundingClientRect().left;
+
+                var middleLineWidth = maxBtnLeft - minBtnLeft;
+                var rightLineWidth = lineWidth - (maxBtnLeft - leftLineCoord) - maxBtnWidth / 2;
 
                 if (coordX - maxBtnWidth >= minBtnCoordX) {
                     if (coordX < lineWidth) {
                         self._lineMiddle.style.width = middleLineWidth + "px";
-                        self._lineRight.style.width = leftLineWidth + "px";
+                        self._lineRight.style.width = rightLineWidth + "px";
+                        self._lineLeft.style.width = lineWidth - rightLineWidth - middleLineWidth + "px";
                     } else {
                         self._lineMiddle.style.width = lineWidth - self._lineLeft.getBoundingClientRect().width + "px";
                         self._lineRight.style.width = "0";
@@ -114,6 +176,15 @@
             };
         }
     }
+
+
+    /**
+     * Возвращает максимальное и минимальное значения
+     * @returns {[number, number]}
+     */
+    DoubleRange.prototype.getValues = function () {
+        return [+this._minOutput.value, +this._maxOutput.value];
+    };
 
     /**
      * Перемещение ползунка на заданное расстояние
@@ -156,6 +227,61 @@
         this._minOutput.value = this._valueMin;
         this._maxOutput.value = this._valueMax;
     };
+
+
+    /**
+     * Проверка _minOutput и _maxOutput на корректность
+     * @returns {boolean}
+     * @private
+     */
+    DoubleRange.prototype._inputValidate = function () {
+        var regexp = /^-?\d+$/;
+
+        // проверка на валидность _minOutput
+        if (!regexp.test(this._minOutput.value)) {
+            if (this._minOutput.value.length === 1) {
+                if (this._minOutput.value !== "-") {
+                    this._minOutput.value = this._min;
+                }
+            } else {
+                this._minOutput.value = this._minOutput.value.slice(0, -1);
+            }
+            return false;
+        }
+
+        if (+this._minOutput.value < this._min || +this._minOutput.value > this._max)
+            return false;
+
+        // проверка на валидность _maxOutput
+        if (!regexp.test(this._maxOutput.value)) {
+            if (this._maxOutput.value.length === 1) {
+                if (this._maxOutput.value !== "-") {
+                    this._maxOutput.value = this._max;
+                }
+            } else {
+                this._maxOutput.value = this._maxOutput.value.slice(0, -1);
+            }
+            return false;
+        }
+
+        if (+this._maxOutput.value < this._min || +this._maxOutput.value > this._max)
+            return false;
+
+        return true;
+    };
+
+
+    /**
+     * Установить ползунок на заданное значение
+     * @param {Element} btn
+     * @param {string} value
+     * @private
+     */
+    DoubleRange.prototype._setBtnPosition = function(btn, value) {
+        var btnWidth = btn.getBoundingClientRect().width;
+        btn.style.left = Math.floor((value - this._min) * this._divVal) - btnWidth / 2 + "px";
+    };
+
 
     window.DoubleRange = DoubleRange;
 
